61c055068509d06a52c31b056801c390ce0e3acb
2No2Name <2No2Name@web.de>
2020-05-17 12:29:24 +0200

    Chunk sections keep track of oversized blocks, reduce number of blocks iterated over when possible

--- src/main/java/me/jellysquid/mods/lithium/mixin/chunk/oversized_blocks/MixinChunkSection.java
+++ src/main/java/me/jellysquid/mods/lithium/mixin/chunk/oversized_blocks/MixinChunkSection.java
@@ -1,5 +1,6 @@
 package me.jellysquid.mods.lithium.mixin.chunk.oversized_blocks;
 
+import me.jellysquid.mods.lithium.common.entity.movement.BlockCollisionSweeper;
 import net.minecraft.block.BlockState;
 import net.minecraft.fluid.FluidState;
 import net.minecraft.world.chunk.ChunkSection;
@@ -7,14 +8,16 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 /**
- * Keep track of how many oversized blocks are in this chunk section. If none are there,
+ * Keep track of how many oversized blocks are in this chunk section. If none are there, collision code can skip a few blocks.
+ * Oversided blocks are fences, walls, extended piston heads and blocks with dynamic bounds (scaffolding, shulker box, moving blocks)
  * @author 2No2Name
  */
 @Mixin(ChunkSection.class)
-public class MixinChunkSection {
+public class MixinChunkSection implements BlockCollisionSweeper.HasOversizedBlocks {
     private short oversizedBlockCount;
 
     //inject into lambda in calculateCounts
@@ -28,9 +31,9 @@ public class MixinChunkSection {
     //      net/minecraft/world/chunk/ChunkSection.method_21731(Lnet/minecraft/block/BlockState;I)V,  <-------------HERE
     //      (Lnet/minecraft/block/BlockState;I)V
     //    ]
-    @Inject(method = "method_21731", at = @At(value = "INVOKE", target = "Lnet/minecraft/fluid/FluidState;hasRandomTicks()Z", shift = At.Shift.AFTER), locals = LocalCapture.CAPTURE_FAILHARD)
+    @Inject(method = "method_21731", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/AbstractBlock$AbstractBlockState;hasRandomTicks()Z", shift = At.Shift.BEFORE), locals = LocalCapture.CAPTURE_FAILHARD)
     private void addToOversizedBlockCount(BlockState blockState, int i, CallbackInfo ci, FluidState fluidState) {
-        if (blockState.exceedsCube() || (!fluidState.isEmpty() && fluidState.getBlockState().exceedsCube())) {
+        if (blockState.exceedsCube()) {
             this.oversizedBlockCount = (short)(this.oversizedBlockCount + i);
         }
     }
@@ -40,5 +43,22 @@ public class MixinChunkSection {
         this.oversizedBlockCount = 0;
     }
 
-    //@Inject(method = "setBlockState(IIILnet/minecraft/block/BlockState;Z)Lnet/minecraft/block/BlockState;", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockState;hasRandomTicks()Z"))
+    @Inject(method = "setBlockState(IIILnet/minecraft/block/BlockState;Z)Lnet/minecraft/block/BlockState;", at = @At(ordinal = 0, value = "INVOKE", target = "Lnet/minecraft/block/BlockState;hasRandomTicks()Z", shift = At.Shift.BEFORE), locals = LocalCapture.PRINT)
+    private void decrOversizedBlockCount(int x, int y, int z, BlockState state, boolean lock, CallbackInfoReturnable<BlockState> cir) {
+        if (state.exceedsCube()) {
+            this.oversizedBlockCount--;
+        }
+    }
+
+    @Inject(method = "setBlockState(IIILnet/minecraft/block/BlockState;Z)Lnet/minecraft/block/BlockState;", at = @At(ordinal = 1, value = "INVOKE", target = "Lnet/minecraft/block/BlockState;hasRandomTicks()Z", shift = At.Shift.BEFORE), locals = LocalCapture.PRINT)
+    private void incrOversizedBlockCount(int x, int y, int z, BlockState state, boolean lock, CallbackInfoReturnable<BlockState> cir) {
+        if (state.exceedsCube()) {
+            this.oversizedBlockCount++;
+        }
+    }
+
+    @Override
+    public boolean hasOversizedBlocks() {
+        return this.oversizedBlockCount > 0;
+    }
 }
